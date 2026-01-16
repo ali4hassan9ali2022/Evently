d3ccb837b10b15fde7c1c01fa3a30c1ef0ec6752
Ali Kasrawy <kasrawyali47@gmail.com>
2026-01-16 11:00:49 +0200

    create app color class

--- lib/Core/utils/app_assets.dart
+++ lib/Core/utils/app_assets.dart
@@ -1,26 +1,124 @@
-abstract final class AppAssets {
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
 
+  /// Assets for imagesAppLogo
+  /// assets/images/app_logo.png
   static const String imagesAppLogo = "assets/images/app_logo.png";
+
+  /// Assets for imagesBeingCreative
+  /// assets/images/being-creative.png
+  static const String imagesBeingCreative = "assets/images/being-creative.png";
+
+  /// Assets for imagesBeingCreative (1)
+  /// assets/images/being-creative (1).png
+  static const String imagesBeingCreative1 =
+      "assets/images/being-creative (1).png";
+
+  /// Assets for imagesBeingCreative (2)
+  /// assets/images/being-creative (2).png
+  static const String imagesBeingCreative2 =
+      "assets/images/being-creative (2).png";
+
+  /// Assets for imagesBirthday
+  /// assets/images/Birthday.png
   static const String imagesBirthday = "assets/images/Birthday.png";
+
+  /// Assets for imagesBirthday1
+  /// assets/images/Birthday-1.png
   static const String imagesBirthday1 = "assets/images/Birthday-1.png";
+
+  /// Assets for imagesBlack White Minimal Modern Simple Bold Business Mag Logofff 1
+  /// assets/images/Black White Minimal Modern Simple Bold Business Mag Logofff 1.png
+  static const String imagesBlackWhite =
+      "assets/images/Black White Minimal Modern Simple Bold Business Mag Logofff 1.png";
+
+  /// Assets for imagesBook Club
+  /// assets/images/Book Club.png
   static const String imagesBookClub = "assets/images/Book Club.png";
+
+  /// Assets for imagesBook Club1
+  /// assets/images/Book Club-1.png
   static const String imagesBookClub1 = "assets/images/Book Club-1.png";
+
+  /// Assets for imagesEvently
+  /// assets/images/Evently.png
+  static const String imagesEvently = "assets/images/Evently.png";
+
+  /// Assets for imagesExhibition
+  /// assets/images/Exhibition.png
   static const String imagesExhibition = "assets/images/Exhibition.png";
+
+  /// Assets for imagesExhibition1
+  /// assets/images/Exhibition-1.png
   static const String imagesExhibition1 = "assets/images/Exhibition-1.png";
+
+  /// Assets for imagesHotTrending
+  /// assets/images/hot-trending.png
+  static const String imagesHotTrending = "assets/images/hot-trending.png";
+
+  /// Assets for imagesIcAll
+  /// assets/images/ic_all.svg
   static const String imagesIcAll = "assets/images/ic_all.svg";
+
+  /// Assets for imagesIcBike
+  /// assets/images/ic_bike.svg
   static const String imagesIcBike = "assets/images/ic_bike.svg";
+
+  /// Assets for imagesIcBitrhday
+  /// assets/images/ic_bitrhday.svg
   static const String imagesIcBitrhday = "assets/images/ic_bitrhday.svg";
+
+  /// Assets for imagesIcBook
+  /// assets/images/ic_book.svg
   static const String imagesIcBook = "assets/images/ic_book.svg";
+
+  /// Assets for imagesIcCalendar
+  /// assets/images/ic_calendar.svg
   static const String imagesIcCalendar = "assets/images/ic_calendar.svg";
+
+  /// Assets for imagesIcEmail
+  /// assets/images/ic_email.svg
   static const String imagesIcEmail = "assets/images/ic_email.svg";
+
+  /// Assets for imagesIcEyeClosed
+  /// assets/images/ic_eye_closed.svg
   static const String imagesIcEyeClosed = "assets/images/ic_eye_closed.svg";
+
+  /// Assets for imagesIcGmail
+  /// assets/images/ic_gmail.svg
   static const String imagesIcGmail = "assets/images/ic_gmail.svg";
+
+  /// Assets for imagesIcLock
+  /// assets/images/ic_lock.svg
   static const String imagesIcLock = "assets/images/ic_lock.svg";
+
+  /// Assets for imagesIcPerson
+  /// assets/images/ic_person.svg
   static const String imagesIcPerson = "assets/images/ic_person.svg";
+
+  /// Assets for imagesIcTime
+  /// assets/images/ic_time.svg
   static const String imagesIcTime = "assets/images/ic_time.svg";
+
+  /// Assets for imagesMeeting
+  /// assets/images/Meeting.png
   static const String imagesMeeting = "assets/images/Meeting.png";
+
+  /// Assets for imagesMeeting1
+  /// assets/images/Meeting-1.png
   static const String imagesMeeting1 = "assets/images/Meeting-1.png";
+
+  /// Assets for imagesRoute logo with arrow design 1
+  /// assets/images/Route logo with arrow design 1.png
+  static const String imagesRoute =
+      "assets/images/Route logo with arrow design 1.png";
+
+  /// Assets for imagesSport
+  /// assets/images/Sport.png
   static const String imagesSport = "assets/images/Sport.png";
+
+  /// Assets for imagesSport1
+  /// assets/images/Sport-1.png
   static const String imagesSport1 = "assets/images/Sport-1.png";
 }
-
