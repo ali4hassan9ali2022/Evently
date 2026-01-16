c616841c593ae019641d7139398591e5e768de0e
Ali Kasrawy <kasrawyali47@gmail.com>
2026-01-16 10:47:06 +0200

    create app Assets

---
+++ lib/Core/utils/app_assets.dart
@@ -0,0 +1,26 @@
+abstract final class AppAssets {
+
+  static const String imagesAppLogo = "assets/images/app_logo.png";
+  static const String imagesBirthday = "assets/images/Birthday.png";
+  static const String imagesBirthday1 = "assets/images/Birthday-1.png";
+  static const String imagesBookClub = "assets/images/Book Club.png";
+  static const String imagesBookClub1 = "assets/images/Book Club-1.png";
+  static const String imagesExhibition = "assets/images/Exhibition.png";
+  static const String imagesExhibition1 = "assets/images/Exhibition-1.png";
+  static const String imagesIcAll = "assets/images/ic_all.svg";
+  static const String imagesIcBike = "assets/images/ic_bike.svg";
+  static const String imagesIcBitrhday = "assets/images/ic_bitrhday.svg";
+  static const String imagesIcBook = "assets/images/ic_book.svg";
+  static const String imagesIcCalendar = "assets/images/ic_calendar.svg";
+  static const String imagesIcEmail = "assets/images/ic_email.svg";
+  static const String imagesIcEyeClosed = "assets/images/ic_eye_closed.svg";
+  static const String imagesIcGmail = "assets/images/ic_gmail.svg";
+  static const String imagesIcLock = "assets/images/ic_lock.svg";
+  static const String imagesIcPerson = "assets/images/ic_person.svg";
+  static const String imagesIcTime = "assets/images/ic_time.svg";
+  static const String imagesMeeting = "assets/images/Meeting.png";
+  static const String imagesMeeting1 = "assets/images/Meeting-1.png";
+  static const String imagesSport = "assets/images/Sport.png";
+  static const String imagesSport1 = "assets/images/Sport-1.png";
+}
+
